6ae1d5d167b5e1413658201e6c8a6d6dbc3880b0
Peter <p.kirby@lancaster.ac.uk>
2021-11-04 22:06:54 +0000

    added gitignore, wrote bash script to import blacklist, updated python to import honeypot IPs

---
+++ call.sh
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level1.netset -o "iplist.txt"
+python3 cross-reference.py
--- cross-reference.py
+++ cross-reference.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 #TODO: read IPs from traffic CSV
-#TODO: download latest list from firehol
+#TODO: download latest list from firehol from https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level1.netset
 #TODO: clean firehol IP list
 #TODO: find intersection of datasets
 
@@ -12,7 +12,14 @@ import pandas as pd
 
 #trafficData = open("../hornet7-netflow-extended/Honeypot-Cloud-DigitalOcean-Geo-1")
 
-my_csv = pd.read_csv("../hornet7-netflow-extended/Honeypot-Cloud-DigitalOcean-Geo-1/2021-04-23_honeypot-cloud-digitalocean-geo-1_netflow-extended.csv")
-column = my_csv.SrcAddr
+path = "../hornet7Dataset/hornet7-netflow-extended/Honeypot-Cloud-DigitalOcean-Geo-1/2021-04-24_honeypot-cloud-digitalocean-geo-1_netflow-extended.csv"
 
-print(column)
+honeypotIPs = pd.read_csv(path, usecols=["SrcAddr"])
+
+blacklistIPfile = open("iplist.txt", "r")
+blacklistIPs = blacklistIPfile.read()
+
+#loop through file and remove info after slashes, also remove lines starting with #
+print(blacklistIPs)
+
+print(honeypotIPs)
