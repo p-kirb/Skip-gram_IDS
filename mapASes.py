152fdcca0bc5814b32ccac5a7ce100b0b42ef37f
Kirby <kirbyp@egb076000003.lancs.local>
2021-11-18 21:07:31 +0000

    finished script to map IP addresses to their ASes and output each day of mappings to new file in dedicated folder

--- mapASes.py
+++ mapASes.py
@@ -1,16 +1,55 @@
 #!/usr/bin/python
 import sys
-
+import pandas as pd
 import pyasn
+import collections
 #use pip install pyasn
 
+
+#writes IPs with slash notation and references as standard ips
+def formatIP(seriesRow):
+    unaltered = seriesRow.values[0]
+    ipWithSources = unaltered.split(",")
+    ipWithMask = ipWithSources[0].split("/")
+
+    ip = ipWithMask[0]
+    return ip
+
 blacklistPath = str(sys.argv[1:][0])
-print("argument: ", blacklistPath)
+date = blacklistPath[len(blacklistPath)-10:len(blacklistPath)]
+asndb_name = "ipasnDBs/ipasn_" + date.replace('-', '') + ".dat"       #making file name
+
 
 #ASN database used to map ips to ASNs
-#asndb = pyasn.pyasn(<created database name>)
+asndb = pyasn.pyasn(asndb_name)
+
+blacklistIPsDF = pd.read_fwf(blacklistPath, header=None)              #dataframe
+blacklistIPsSeries = blacklistIPsDF.squeeze(axis=0)     #series
+#cleaning blacklist IPs
+blacklistIPsSeries = blacklistIPsSeries.apply(formatIP, axis=1)
+blacklistIPs = blacklistIPsSeries.tolist()
+
+asnList = []
+for ip in blacklistIPs:
+    asnList.append(asndb.lookup(ip)[0])
+
+asnFrequencies = collections.Counter(asnList)
+sortedFreq = {}
+sortedKeys = sorted(asnFrequencies, key=asnFrequencies.get)  # [1, 3, 2]
+
+for w in sortedKeys:
+    sortedFreq[w] = asnFrequencies[w]
 
 
+with open("freq_of_ASNs_on_blacklists/ASN frequencies" + date + ".txt", "w") as file:
+    file.write("ASN: Frequency\n")
+    for key, value in sortedFreq.items():
+        line = str(key) + ": " + str(value) + "\n"
+        file.write(line)
+
+
+#print(asnFrequencies)
+#    print(asndb.lookup(ip))
 #loop through each IP in cleaned list and call asndb.lookup(<ip string>) to get list of ASNs
 
 
--- map_IPs_to_AS.sh
+++ map_IPs_to_AS.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
+pip install pyasn
 cd ../
 mkdir week_of_blacklists
 cd week_of_blacklists
 #download zip lists
-#curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-23.zip -o "day1.zip"
-#curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-24.zip -o "day2.zip"
-#curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-25.zip -o "day3.zip"
-#curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-26.zip -o "day4.zip"
-#curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-27.zip -o "day5.zip"
-#curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-28.zip -o "day6.zip"
-#curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-29.zip -o "day7.zip"
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-23.zip -o "day1.zip"
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-24.zip -o "day2.zip"
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-25.zip -o "day3.zip"
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-26.zip -o "day4.zip"
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-27.zip -o "day5.zip"
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-28.zip -o "day6.zip"
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-29.zip -o "day7.zip"
 
 unzip day1.zip
 unzip day2.zip
@@ -30,10 +31,31 @@ rm -r 2021-04-23    2021-04-24    2021-04-25    2021-04-26    2021-04-27    2021
 mkdir zips
 mv *.zip zips
 
-#downloading RIB files (used to build database to map IPs to ASNs with pyasn module)
-python3 ~/.local/bin/pyasn_util_download.py --dates-from-file dates
-
-
-
 cd ../SCC300HoneypotProject
+#downloading RIB files (used to build database to map IPs to ASNs with pyasn module)
+python3 ~/.local/bin/pyasn_util_download.py --dates-from-file dates.txt
+
+#moving downloaded files to folder
+mkdir bz2_files
+mv *.bz2 bz2_files
+
+#moving databases to folder
+mkdir ipasnDBs
+mv *.dat ipasnDBs
+
+#converting .bz2 files to database ".dat" files
+python3 ~/.local/bin/pyasn_util_convert.py --bulk 2021-04-23 2021-04-29
+
+#making folder for output of python files
+mkdir freq_of_ASNs_on_blacklists
 python3 mapASes.py ../week_of_blacklists/lists/2021-04-23
+python3 mapASes.py ../week_of_blacklists/lists/2021-04-24
+python3 mapASes.py ../week_of_blacklists/lists/2021-04-25
+python3 mapASes.py ../week_of_blacklists/lists/2021-04-26
+python3 mapASes.py ../week_of_blacklists/lists/2021-04-27
+python3 mapASes.py ../week_of_blacklists/lists/2021-04-28
+python3 mapASes.py ../week_of_blacklists/lists/2021-04-29
+
+
+
+
