a60fda3bbf0d8acf63f92e408522cef4e70d2b6d
Kirby <kirbyp@egb076000008.lancs.local>
2021-11-25 15:05:21 +0000

    fixed script

--- Mapping_IPs_to_Autonomous_Systems/map_IPs_to_AS.sh
+++ Mapping_IPs_to_Autonomous_Systems/map_IPs_to_AS.sh
@@ -1,61 +1,65 @@
-#!/bin/sh
-pip install pyasn
-cd ../../
-mkdir week_of_blacklists
-cd week_of_blacklists
-#download zip lists
-curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-23.zip -o "day1.zip"
-curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-24.zip -o "day2.zip"
-curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-25.zip -o "day3.zip"
-curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-26.zip -o "day4.zip"
-curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-27.zip -o "day5.zip"
-curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-28.zip -o "day6.zip"
-curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-29.zip -o "day7.zip"
+#!/bin/sh 
+pip install pyasn 
+pip install pandas 
 
-unzip day1.zip
-unzip day2.zip
-unzip day3.zip
-unzip day4.zip
-unzip day5.zip
-unzip day6.zip
-unzip day7.zip
+#data stored outside of git folder 
+cd ../../ 
+mkdir week_of_blacklists 
+cd week_of_blacklists 
+#download zip lists 
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-23.zip -o "day1.zip" 
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-24.zip -o "day2.zip" 
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-25.zip -o "day3.zip" 
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-26.zip -o "day4.zip" 
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-27.zip -o "day5.zip" 
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-28.zip -o "day6.zip" 
+curl https://steel.isi.edu/projects/BLAG/data/2021/04/2021-04-29.zip -o "day7.zip" 
 
-#move plaintext lists to lists folder
-mkdir lists
-mv 2021-04-23/2021-04-23    2021-04-24/2021-04-24   2021-04-25/2021-04-25   2021-04-26/2021-04-26   2021-04-27/2021-04-27   2021-04-28/2021-04-28   2021-04-29/2021-04-29 lists
+unzip day1.zip 
+unzip day2.zip 
+unzip day3.zip 
+unzip day4.zip 
+unzip day5.zip 
+unzip day6.zip 
+unzip day7.zip 
 
-#remove folders
-rm -r 2021-04-23    2021-04-24    2021-04-25    2021-04-26    2021-04-27    2021-04-28    2021-04-29
+#move plaintext lists to lists folder 
+mkdir lists 
+mv 2021-04-23/2021-04-23    2021-04-24/2021-04-24   2021-04-25/2021-04-25   2021-04-26/2021-04-26   2021-04-27/2021-04-27   2021-04-28/2021-04-28   2021-04-29/2021-04-29 lists 
 
-#moving zips into their own folder
-mkdir zips
-mv *.zip zips
+#remove folders 
+rm -r 2021-04-23    2021-04-24    2021-04-25    2021-04-26    2021-04-27    2021-04-28    2021-04-29 
 
-cd ../SCC300HoneypotProject/Mapping_IPs_to_Autonomous_Systems
-#downloading RIB files (used to build database to map IPs to ASNs with pyasn module)
-python3 ~/.local/bin/pyasn_util_download.py --dates-from-file dates.txt
+#moving zips into their own folder 
+mkdir zips 
+mv *.zip zips 
 
-#moving downloaded files to folder
-mkdir bz2_files
-mv *.bz2 bz2_files
+cd ../SCC300HoneypotProject/Mapping_IPs_to_Autonomous_Systems 
 
-#moving databases to folder
-mkdir ipasnDBs
-mv *.dat ipasnDBs
+#downloading RIB files (used to build database to map IPs to ASNs with pyasn module) 
+python3 ~/.local/bin/pyasn_util_download.py --dates-from-file dates.txt 
 
-#converting .bz2 files to database ".dat" files
-python3 ~/.local/bin/pyasn_util_convert.py --bulk 2021-04-23 2021-04-29
+#converting .bz2 files to database ".dat" files 
+python3 ~/.local/bin/pyasn_util_convert.py --bulk 2021-04-23 2021-04-29 
 
-#making folder for output of python files
-mkdir freq_of_ASNs_on_blacklists
-python3 mapASes.py ../../week_of_blacklists/lists/2021-04-23
-python3 mapASes.py ../../week_of_blacklists/lists/2021-04-24
-python3 mapASes.py ../../week_of_blacklists/lists/2021-04-25
-python3 mapASes.py ../../week_of_blacklists/lists/2021-04-26
-python3 mapASes.py ../../week_of_blacklists/lists/2021-04-27
-python3 mapASes.py ../../week_of_blacklists/lists/2021-04-28
-python3 mapASes.py ../../week_of_blacklists/lists/2021-04-29
+#moving downloaded files to folder 
+mkdir bz2_files 
+mv *.bz2 bz2_files 
 
+#moving databases to folder 
+mkdir ipasnDBs 
+mv *.dat ipasnDBs 
+
+
+#making folder for output of python files 
+mkdir freq_of_ASNs_on_blacklists 
+python3 mapASes.py ../../week_of_blacklists/lists/2021-04-23 
+python3 mapASes.py ../../week_of_blacklists/lists/2021-04-24 
+python3 mapASes.py ../../week_of_blacklists/lists/2021-04-25 
+python3 mapASes.py ../../week_of_blacklists/lists/2021-04-26 
+python3 mapASes.py ../../week_of_blacklists/lists/2021-04-27 
+python3 mapASes.py ../../week_of_blacklists/lists/2021-04-28 
+python3 mapASes.py ../../week_of_blacklists/lists/2021-04-29 
 
 
 
